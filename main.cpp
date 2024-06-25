ae34c83d961d698e91d194634b64cf82571f6cd9
jarocutipam <jarocutipam@unjbg.edu.pe>
2024-06-25 01:36:18 -0500

    primer commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include"funciones.h"
+
+using namespace std;
+
+int main() {
+	int a=10, b=5;
+	cout<<"suma:"<<suma(a,b)<<endl;
+	cout<<"resta:"<<resta(a,b)<<endl;
+	return 0;
+}
